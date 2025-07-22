900bcd8b9e8109ca4fb64e5c367fe15e389e4a07
Felipe Martins <fo729183@gmail.com>
2025-07-22 17:06:24 -0300

    refactor: fixing the sidebar and icons

--- src/components/Sidebar/sidebar.tsx
+++ src/components/Sidebar/sidebar.tsx
@@ -1,12 +1,36 @@
+'use client'
+
+import { RiInboxArchiveLine, 
+    RiHeart2Line, 
+    RiArrowRightCircleLine, 
+    RiEdit2Line } from "react-icons/ri";
+
+
 import Styles from "./sidebar.module.css"
-import Buttons from "../Buttons/buttons"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
+const pathname = usePathname()
+
     return (
-        <div className={Styles.container}>
-            <Buttons label="Arquivos"></Buttons>
-            <Buttons label="Favoritos"></Buttons>
-            <Buttons label="Editor"></Buttons>
-        </div>
+        <nav className={Styles.nav}>
+            <ul>
+                <li>
+                    <Link href='/' className={pathname === '/' ? Styles.active : ''}>
+                        <RiInboxArchiveLine/>
+                        All files
+                    </Link>
+                    <Link href='/favorites' className={pathname === '/favorites' ? Styles.active : ''}>
+                        <RiHeart2Line />
+                        Favorites
+                    </Link>
+                    <Link href='/editor' className={pathname === '/editor' ? Styles.active : ''}>
+                        <RiEdit2Line />
+                        Editor
+                    </Link>
+                </li>
+            </ul>
+        </nav>
     );
 };
